cd11ba3883c2c3682a66da8954387419700ffd01
baominh5xx2 <baominh5xx2@gmail.com>
2024-12-13 22:57:23 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import React, { Fragment } from 'react'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import routes from './routes/index'
-import DefaultComponent from './components/DefaultComponent/DefaultComponent'
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import routes from './routes/index';
+import DefaultComponent from './components/DefaultComponent/DefaultComponent';
+import ProtectedRoute from './components/ProtectedRoute';
+import SignUp from './pages/SignUp/SignUp';
+import SignUpDetails from './pages/SignUp/SignUp_next';
 
 function App() {
   return (
-    <div>
+    <AuthProvider>
+      <div>
         <Router>
           <Routes>
-            { routes.map((route) => {
-              const Page = route.page
+            {/* Add signup routes first */}
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signup-details" element={<SignUpDetails />} />
+            
+            {/* Map through other routes */}
+            {routes.map((route) => {
+              const Page = route.page;
               const Layout = route.isShowHeader ? DefaultComponent : Fragment;
+              
               return (
-                <Route key={route.path} path={route.path} element={
-                  <Layout>
-                    <Page/>
-                  </Layout>
-                } />
-              )
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  element={
+                    route.protected ? (
+                      <ProtectedRoute>
+                        <Layout>
+                          <Page />
+                        </Layout>
+                      </ProtectedRoute>
+                    ) : (
+                      <Layout>
+                        <Page />
+                      </Layout>
+                    )
+                  }
+                />
+              );
             })}
+            
+            {/* Fallback route */}
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </Router>
-    </div>
-  )
+      </div>
+    </AuthProvider>
+  );
 }
 
 export default App;
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,47 +7,128 @@ import DetailEmployee from '../pages/DetailEmployee/DetailEmployee'
 import ImportProduct from '../pages/ImportProductPage/ImportProduct'
 import CreateImportProduct from '../pages/CreateImportProduct/CreateImportProduct'
 import DetailImportProduct from '../pages/DetailImportProduct/DetailImportProduct'
+import AdjustServicePage from '../pages/AdjustServicePage/AdjustServicePage';
+import OrderProductPage from '../pages/OrderProductPage/OrderProductPage';
+import TopbarComponent from '../components/TopbarComponent/TopbarComponent';
+import ProductPage from '../pages/ProductPage/ProductPage';
+import ServicePage from '../pages/ServicePage/ServicePage';
+import AddServicePage from '../pages/AddServicePage/AddServicePage';
+import AddProductPage from '../pages/AddProductPage/AddProductPage';
+import AdjustProductPage from '../pages/AdjustProductPage/AdjustProductPage'; // Import AdjustProductPage
+import SignIn from '../pages/SignIn/SignIn';
+import SignUp from '../pages/SignUp/SignUp';  
+import SignUpDetails from '../pages/SignUp/SignUp_next'; // Import SignUpDetails
 
 const routes = [
     {
-        path:'/',
-        page: HomePage,
-        isShowHeader: true
-    },
-    {
         path: 'list-customer',
         page: ListCustomerPage,
-        isShowHeader: true
+        isShowHeader: true,
+        protected: true,
     },
     {
         path: 'customer-detail/1',
         page: DetailCustomer,
-        isShowHeader: true
+        isShowHeader: true,
+        protected: true,
     },
     {
         path: 'list-employee',
         page: ListEmployeePage,
-        isShowHeader: true
+        isShowHeader: true,
+        protected: true,
     },
     {
         path: 'employee-detail/1',
         page: DetailEmployee,
-        isShowHeader: true
+        isShowHeader: true,
+        protected: true,
     },
     {
         path: 'list-import-product',
         page: ImportProduct,
-        isShowHeader: true
+        isShowHeader: true,
+        protected: true,
     },
     {
         path: 'import-product-detail/1',
         page: DetailImportProduct,
-        isShowHeader: true
+        isShowHeader: true,
+        protected: true,
     },
     {
         path: 'create-import-product',
         page: CreateImportProduct,
-        isShowHeader: true
+        isShowHeader: true,
+        protected: true,
+    },
+    {
+        path: '/dang-nhap',
+        page: SignIn,
+        isShowHeader: false,
+        protected: false,
+    },
+    {
+        path: '/dang-ky',
+        page: SignUp,
+        isShowHeader: false,
+        protected: false,
+    },
+    {
+        path: '/',
+        page: HomePage,
+        isShowHeader: true,
+        protected: true,
+    },
+    {
+        path: 'adjust-service/:id',
+        page: AdjustServicePage,
+        isShowHeader: true,
+        protected: true,
+    },
+    {
+        path: 'list-order-product',
+        page: OrderProductPage,
+        isShowHeader: true,
+        protected: true,
+    },
+    {
+        path: 'list-product',
+        page: ProductPage,
+        isShowHeader: true,
+        protected: true,
+    },
+    {
+        path: 'list-service',
+        page: ServicePage,
+        isShowHeader: true,
+        protected: true,
+    },
+    {
+        path: 'add-product',
+        page: AddProductPage,
+        isShowHeader: true,
+        protected: true,
+    },
+    {
+        path: 'adjust-product/:key', // Thêm route cho AdjustProductPage với tham số id
+        page: AdjustProductPage,
+        isShowHeader: true,
+        protected: true,
+    },
+    {
+        path: 'add-service',
+        page: AddServicePage,
+        isShowHeader: true,
+        protected: true,
+    },
+    {
+        path: 'top-bar',
+        page: TopbarComponent,
+    },
+    {
+        path: 'signup-details',
+        page: SignUpDetails,
     },
     {
         path: '*',
